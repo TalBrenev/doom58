2095c6fc594a03a6177d9d0b4b2f79f5b953f780
Tal Brenev <tal.brenev@gmail.com>
2019-07-04 18:53:52 -0400

    Add reset signals to interfaces

--- raytracer.v
+++ raytracer.v
@@ -1,2 +1,2 @@
-module raytracer(clock, start, x, y, angle, grid_x, grid_y, grid_write, grid_out, done);
+module raytracer(clock, reset, start, x, y, angle, grid_x, grid_y, grid_write, grid_out, done);
 endmodule
