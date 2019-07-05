455b9aea9b9d621aeb8559c4c493a5bfb18f2e87
Tal Brenev <tal.brenev@gmail.com>
2019-07-05 17:04:30 -0400

    Remove unnecessary space

--- .github/lint/lint.sh
+++ .github/lint/lint.sh
@@ -3,10 +3,10 @@
 verilator --lint-only *.v > results.txt 2>&1
 
 if [ -z "$(cat results.txt)" ] ; then
-    echo "Lint on branch" $GITHUB_REF " passed!"
+    echo "Lint on branch" $GITHUB_REF "passed!"
     exit 0
 else
-    echo "Lint on branch" $GITHUB_REF " failed:"
+    echo "Lint on branch" $GITHUB_REF "failed:"
     cat results.txt
     exit 1
 fi
