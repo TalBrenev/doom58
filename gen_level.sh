78bed80078b7868da149dce5f6b2625b199b747e
Tal Brenev <tal.brenev@gmail.com>
2019-07-08 22:22:53 -0400

    Move verilog generators to the proper place

--- gen_level.sh
+++ gen_level.sh
@@ -4,5 +4,5 @@
 # ./gen_level.sh 0 1 2
 
 for num in $@; do
-    python3 gen_level.py "levels/$num.txt" "level_$num" > "level_$num.v"
+    python3 verilog_generators/gen_level.py "levels/$num.txt" "level_$num" > "level_$num.v"
 done
