3447c4ed870d9c39f5f4ef02c7cfa210e63bf14a
moladan123 <alon.djurinsky@gmail.com>
2019-07-22 17:48:17 -0400

    fixed linter warnings?

--- player_updater.v
+++ player_updater.v
@@ -36,12 +36,12 @@ module player_updater(clock, reset,
    output [5:0] grid_x;
    output [4:0] grid_y;
    input [2:0] grid_out;
-	coordinate_to_grid var1 (cur_pos_x, cur_pos_y, grid_x, grid_y);
+	coordinate_to_grid var1 (cur_pos_x [14:0], cur_pos_y[13:0], grid_x[5:0], grid_y[4:0]);
 	
 	// get direction vector
 	wire [15:0] direction_x;
 	wire [14:0] direction_y;
-	bytian_to_vector var2 (cur_angle, direction_x, direction_y);
+	bytian_to_vector var2 (cur_angle, direction_x[14:0], direction_y[13:0]);
 	
 	/*
 		1 get theoretical location
@@ -60,13 +60,13 @@ module player_updater(clock, reset,
 	reg [3:0] cur_state, next_state;
 	always @(*) begin // state stable
 		case (cur_state)
-		WAIT: next_state <= start && (counter[19:0] == 0) ? PREDICT_LOCATION : WAIT;
-		PREDICT_LOCATION: next_state <= SEND_COORD;
-		SEND_COORD: next_state <= GET_COORD;
-		GET_COORD: next_state <= MOVE;
-		MOVE: next_state <= DONE;
-		DONE: next_state <= WAIT;
-		default: next_state <= WAIT;
+		WAIT: next_state = start && (counter[19:0] == 0) ? PREDICT_LOCATION : WAIT;
+		PREDICT_LOCATION: next_state = SEND_COORD;
+		SEND_COORD: next_state = GET_COORD;
+		GET_COORD: next_state = MOVE;
+		MOVE: next_state = DONE;
+		DONE: next_state = WAIT;
+		default: next_state = WAIT;
 		endcase
 	end
 	
@@ -87,29 +87,29 @@ module player_updater(clock, reset,
 			done = 1'b0;
 			case (movement)
 				RIGHT: begin 
-					temp_angle <= cur_angle + {4'b0, turn_speed}; 
-					temp_pos_x <= cur_pos_x;
-					temp_pos_y <= cur_pos_y;
+					temp_angle = cur_angle + {4'b0, turn_speed}; 
+					temp_pos_x = cur_pos_x;
+					temp_pos_y = cur_pos_y;
 				end
 				LEFT: begin 
-					temp_angle <= cur_angle - {4'b0, turn_speed}; 
-					temp_pos_x <= cur_pos_x;
-					temp_pos_y <= cur_pos_y;
+					temp_angle = cur_angle - {4'b0, turn_speed}; 
+					temp_pos_x = cur_pos_x;
+					temp_pos_y = cur_pos_y;
 				end
 				UP: begin 
-					temp_angle <= cur_angle; 
-					temp_pos_x <= cur_pos_x + direction_x;
-					temp_pos_y <= cur_pos_y + direction_y;
+					temp_angle = cur_angle; 
+					temp_pos_x = cur_pos_x + direction_x;
+					temp_pos_y = cur_pos_y + direction_y;
 				end
 				DOWN: begin
-					temp_angle <= cur_angle; 
-					temp_pos_x <= cur_pos_x - direction_x;
-					temp_pos_y <= cur_pos_y - direction_y;
+					temp_angle = cur_angle; 
+					temp_pos_x = cur_pos_x - direction_x;
+					temp_pos_y = cur_pos_y - direction_y;
 				end
 				default: begin
-					temp_angle <= cur_angle;
-					temp_pos_x <= cur_pos_x;
-					temp_pos_y <= cur_pos_y;
+					temp_angle = cur_angle;
+					temp_pos_x = cur_pos_x;
+					temp_pos_y = cur_pos_y;
 				end
 			endcase
 		end
@@ -117,14 +117,14 @@ module player_updater(clock, reset,
 			begin
 			case (grid_out)
 			3'b000: begin
-					next_angle <= temp_angle;
-					next_pos_x <= temp_pos_x;
-					next_pos_y <= temp_pos_y;
+					next_angle = temp_angle;
+					next_pos_x = temp_pos_x;
+					next_pos_y = temp_pos_y;
 				end
 			default: begin
-					next_angle <= cur_angle;
-					next_pos_x <= cur_pos_x;
-					next_pos_y <= cur_pos_y;
+					next_angle = cur_angle;
+					next_pos_x = cur_pos_x;
+					next_pos_y = cur_pos_y;
 				end
 			endcase
 		end
@@ -132,6 +132,11 @@ module player_updater(clock, reset,
 		DONE: begin
 			done <= 1'b1;
 		end
+
+		default: begin
+			done <= 1'b1;
+		end
+
 	endcase
 	end
 	
