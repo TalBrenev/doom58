c85546f59bb71b9c7da65765e74dcaf6b64a999e
Tal Brenev <tal.brenev@gmail.com>
2019-07-22 16:12:05 -0400

    Uncomment module

--- enemy_updater.v
+++ enemy_updater.v
@@ -1,15 +1,15 @@
-//module enemy_updater(clock, reset,
-//                     start, done,
-//                     grid_x, grid_y, grid_out, grid_write, grid_in);
-//    input clock;
-//    input reset;
-//
-//    input start;
-//    output done;
-//
-//    output [5:0] grid_x;
-//    output [4:0] grid_y;
-//    input [2:0] grid_out;
-//    output grid_write;
-//    output [2:0] grid_in;
-//endmodule
+module enemy_updater(clock, reset,
+                     start, done,
+                     grid_x, grid_y, grid_out, grid_write, grid_in);
+    input clock;
+    input reset;
+
+    input start;
+    output done;
+
+    output [5:0] grid_x;
+    output [4:0] grid_y;
+    input [2:0] grid_out;
+    output grid_write;
+    output [2:0] grid_in;
+endmodule
