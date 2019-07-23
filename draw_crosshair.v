2edf87dcbaaba55e770cce6813f5d38ab64964bb
Tal Brenev <tal.brenev@gmail.com>
2019-07-23 18:25:52 -0400

    Fix crosshair

--- draw_crosshair.v
+++ draw_crosshair.v
@@ -28,19 +28,19 @@ module draw_crosshair(clock, reset,
     reg [3:0] state;
 
     // Flip-flop assignments
-    localparam WAIT            = 4'd0,
-               INITIALIZE      = 4'd1,
-               DRAW_MIDDLE     = 4'd2, // draw the middle square
-               WAIT_MIDDLE     = 4'd3, // wait for the VGA to draw the middle square
-               DRAW_TOP        = 4'd4,
-               WAIT_TOP        = 4'd5,
-               DRAW_RIGHT      = 4'd6,
-               WAIT_RIGHT      = 4'd7,
-               DRAW_BOTTOM     = 4'd8,
-               WAIT_BOTTOM     = 4'd9,
-               DRAW_LEFT       = 4'd10,
-               WAIT_LEFT       = 4'd11,
-               DONE            = 4'd12;
+    localparam WAIT          = 4'd0,
+               INITIALIZE    = 4'd1,
+               DRAW_MIDDLE   = 4'd2, // draw the middle square
+               UPDATE_MIDDLE = 4'd3, // wait for the VGA to draw the middle square
+               DRAW_TOP      = 4'd4,
+               UPDATE_TOP    = 4'd5,
+               DRAW_RIGHT    = 4'd6,
+               UPDATE_RIGHT  = 4'd7,
+               DRAW_BOTTOM   = 4'd8,
+               UPDATE_BOTTOM = 4'd9,
+               DRAW_LEFT     = 4'd10,
+               UPDATE_LEFT   = 4'd11,
+               DONE          = 4'd12;
 
     // state assignments
     always @(posedge clock) begin
@@ -48,17 +48,23 @@ module draw_crosshair(clock, reset,
             state <= WAIT;
         else begin
             case (state)
-                WAIT:            state <= start ? INITIALIZE : WAIT;
-                DRAW_MIDDLE:     state <= WAIT_MIDDLE;
-                WAIT_MIDDLE:     state <= draw_square_done ? DRAW_TOP : WAIT_MIDDLE;
-                DRAW_TOP:        state <= WAIT_MIDDLE;
-                WAIT_TOP:        state <= draw_square_done ? DRAW_RIGHT : WAIT_MIDDLE;
-                DRAW_RIGHT:      state <= WAIT_MIDDLE;
-                WAIT_RIGHT:      state <= draw_square_done ? DRAW_BOTTOM : WAIT_MIDDLE;
-                DRAW_BOTTOM:     state <= WAIT_MIDDLE;
-                WAIT_BOTTOM:     state <= draw_square_done ? DRAW_LEFT : WAIT_MIDDLE;
-                DRAW_LEFT:       state <= WAIT_MIDDLE;
-                WAIT_LEFT:       state <= draw_square_done ? DONE : WAIT_MIDDLE;
+                WAIT:            state <= start ?  UPDATE_MIDDLE : WAIT;
+
+                UPDATE_MIDDLE:   state <= DRAW_MIDDLE;
+                DRAW_MIDDLE:     state <= UPDATE_TOP;
+
+                UPDATE_TOP:      state <= DRAW_TOP;
+                DRAW_TOP:        state <= UPDATE_RIGHT;
+
+                UPDATE_RIGHT:    state <= DRAW_RIGHT;
+                DRAW_RIGHT:      state <= UPDATE_BOTTOM;
+
+                UPDATE_BOTTOM:   state <= DRAW_BOTTOM;
+                DRAW_BOTTOM:     state <= UPDATE_LEFT;
+
+                UPDATE_LEFT:     state <= DRAW_LEFT;
+                DRAW_LEFT:       state <= DONE;
+
                 DONE:            state <= WAIT;
                 default:         state <= WAIT;
             endcase
@@ -68,39 +74,36 @@ module draw_crosshair(clock, reset,
     // output signal logic
     assign done = state == DONE;
 
-    // tell the vga when we want to draw to it
-    wire draw_square_done;
-    
     assign vga_write = (state == DRAW_MIDDLE) || (state == DRAW_TOP) || (state == DRAW_RIGHT) || (state == DRAW_BOTTOM) || (state == DRAW_LEFT);
 
     /* ************** DATAPATH *************** */
     // decide which pixel to draw to
 
     // this implementation could potentially overflow
-    // will not happen so long as all edges of the map are surrounded by walls 
+    // will not happen so long as all edges of the map are surrounded by walls
     always @(*) begin
         case (state)
-        DRAW_MIDDLE:
+        UPDATE_MIDDLE:
             begin
                 vga_x[7:0] = center_x;
                 vga_y[6:0] = center_y;
             end
-        DRAW_TOP:
+        UPDATE_TOP:
             begin
                 vga_x[7:0] = center_x;
                 vga_y[6:0] = center_y - 1;
             end
-        DRAW_RIGHT:
+        UPDATE_RIGHT:
             begin
                 vga_x[7:0] = center_x + 1;
                 vga_y[6:0] = center_y;
             end
-        DRAW_BOTTOM:
+        UPDATE_BOTTOM:
             begin
                 vga_x[7:0] = center_x;
                 vga_y[6:0] = center_y + 1;
             end
-        DRAW_LEFT:
+        UPDATE_LEFT:
             begin
                 vga_x[7:0] = center_x - 1;
                 vga_y[6:0] = center_y;
@@ -112,7 +115,4 @@ module draw_crosshair(clock, reset,
             end
         endcase
     end
-
-
-    
 endmodule
