e0e7d4fa3f31c1183d1fe27bc820abb04a0a511f
Tal Brenev <tal.brenev@gmail.com>
2019-07-24 16:56:48 -0400

    Modify interface for drawing line

--- draw_vertical_line.v
+++ draw_vertical_line.v
@@ -1,7 +1,7 @@
 module draw_vertical_line(clock, reset,
-                      start, done,
-                      curr_x, min_y, max_y,
-                      vga_x, vga_y, vga_colour, vga_write);
+                          start, done,
+                          x, min_y, max_y, colour,
+                          vga_x, vga_y, vga_colour, vga_write);
 
     // Global clock and reset
     input clock;
@@ -11,19 +11,15 @@ module draw_vertical_line(clock, reset,
     input start;
     output done;
 
-    // The center of the crosshair to be drawn
-    // IMPORTANT NOTE: This is already in pixels, i.e. the location on the VGA display
-    input [7:0] curr_x;
-    input [6:0] max_y;
-    input [6:0] min_y;
+    // Inputs
+    input [7:0] x;        // Horizontal position of line
+    input [6:0] max_y;    // Starting y-coordinates
+    input [6:0] min_y;    // Ending y-coordinate
+    input [17:0] colour;  // Colour of line
 
     // Signals to the VGA adapter
-    output reg [7:0] vga_x;
-    output reg [6:0] vga_y;
-    output [17:0] vga_colour; assign vga_colour = 18'b000111000111000111;
+    output [7:0] vga_x;
+    output [6:0] vga_y;
+    output [17:0] vga_colour;
     output vga_write;
-
-
-
-
 endmodule
