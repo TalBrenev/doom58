2b4598a891e1d0fd902966a98292274345cab8b0
moladan123 <alon.djurinsky@gmail.com>
2019-07-25 13:56:57 -0400

    implemented interface

--- draw_vertical_line.v
+++ draw_vertical_line.v
@@ -22,4 +22,65 @@ module draw_vertical_line(clock, reset,
     output [6:0] vga_y;
     output [17:0] vga_colour;
     output vga_write;
+
+
+    reg [2:0] state;
+
+    // Flip-flop assignments
+    localparam WAIT          = 3'd0,
+               INITIALIZE    = 3'd1,
+               UPDATE          = 3'd2, // draw the middle square
+               DRAW        = 3'd3,
+               INCREMENT     = 3'd4, // wait for the VGA to draw the middle square
+               DONE          = 3'd5;
+
+    // state assignments
+    always @(posedge clock) begin
+        if (reset)
+            state <= WAIT;
+        else begin
+            case (state)
+                WAIT:            state <= start ? INITIALIZE : WAIT;
+                INITIALIZE:      state <= UPDATE;
+
+                UPDATE:          state <= DRAW;
+                DRAW:            state <= INCREMENT;
+                INCREMENT:       state <= (curr_y < max_y) ? UPDATE : DONE;
+
+                DONE:            state <= WAIT;
+                default:         state <= WAIT;
+            endcase
+        end
+    end
+
+    // local values
+    reg [6:0] curr_y;
+
+    // output signal logic
+    assign done = state == DONE;
+    assign vga_write = state == DRAW;
+
+    /* -----------DATAPATH------------ */
+
+    always @(posedge clock) begin
+        case (state)
+        INITIALIZE:
+            begin
+                curr_y <= min_y;
+            end
+
+        UPDATE:
+            begin
+                vga_x[7:0] <= x;
+                vga_y[6:0] <= curr_y;
+                vga_colour <= colour;
+            end
+
+        INCREMENT:
+            begin
+                 curr_y <= curr_y + 1;
+            end
+        endcase
+    end
+
 endmodule
